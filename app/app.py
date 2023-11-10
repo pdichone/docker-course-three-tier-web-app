586983a249c41b342636312a5cd4c7af3e400bb6
Paulo Dichone <pdichone@Paulos-MacBook-Pro.local>
2023-11-10 13:12:25 -0800

    testing still..

--- app/app.py
+++ app/app.py
@@ -80,5 +80,5 @@ def index():
 
 # new
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5002)
+    app.run(host="0.0.0.0", port=5000)
     # app.run(debug=True, host="0.0.0.0", port=5002)
